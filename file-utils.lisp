093eb96ee285f9073294e7bc60a0fb7fb6f57598
Matthew Veety <mveety@gmail.com>
2014-11-10 19:04:01 -0500

    refactoring

--- file-utils.lisp
+++ file-utils.lisp
@@ -12,55 +12,6 @@
 ;;     * build targets (ala make) (could be a plugin)
 ;;     * testing. It works for me, it might not for you
 
-(defparameter *variables* '(prefix "/usr/local"
-			    bindir "/usr/local/bin"
-			    libdir "/usr/local/lib"
-			    etcdir "/usr/local/etc"
-			    default-mode 555))
-
-(defun get-var (varname)
-  (getf *variables* varname))
-
-(defun set-var (varname value)
-  (let* ((vstat (getf *variables* varname)))
-    (if (equal vstat nil)
-	(progn
-	  (pushnew value *variables*)
-	  (pushnew varname *variables*))
-	(setf (getf *variables* varname) value))))
-
-(defun parg (arg)
-  (if (symbolp arg)
-      (getf *variables* arg)
-      arg))
-
-(defun string-to-symbol (string)
-  (if (stringp string)
-      (intern (string-upcase string))
-      nil))
-
-(defun string-to-keyword (string)
-  (if (stringp string)
-      (intern (string-upcase string) "KEYWORD")
-      nil))
-
-(defun symbol-to-keyword (symbol)
-  (if (symbolp symbol)
-      (let* ((s (string symbol)))
-	(intern (string-upcase s) "KEYWORD"))
-      nil))
-
-(defun varhdl (form)
-  (if (symbolp form)
-      (let ((bf (getf *variables* form)))
-	(if (not (equal bf nil))
-	    bf
-	    form))
-      form))
-
-(defmacro nilp (form)
-  `(equal ,form nil))
-
 (defun pl-exec (args)
   (if (listp args)
       (let* ((cmd (car args))
