18bbbe15ef8b0aab5d7bdd4aa794ef8dddb6af90
Matthew Veety <mveety@gmail.com>
2021-03-13 19:08:14 -0500

    install prints permissions as a number instead of using the internal representation. added functions to convert permissions from strings of numbers to the internal representation. install is able to take either a string or numbers or a list for permissions

--- file-utils.lisp
+++ file-utils.lisp
@@ -18,6 +18,56 @@
 	     (cmd-args (cdr args)))
 	(oth-run-executable cmd cmd-args))))
 
+(defun perms-to-number (perms-list)
+  (let ((user 0)
+	(group 0)
+	(other 0))
+    (dolist (x perms-list)
+      (cond
+	((equal x :user-read) (incf user 4))
+	((equal x :user-write) (incf user 2))
+	((equal x :user-exec) (incf user 1))
+	((equal x :group-read) (incf group 4))
+	((equal x :group-write) (incf group 2))
+	((equal x :group-exec) (incf group 1))
+	((equal x :other-read) (incf other 4))
+	((equal x :other-write) (incf other 2))
+	((equal x :other-exec) (incf other 1))))
+    (format nil "~A~A~A" user group other)))
+
+(defun number-to-perms (perms-str)
+  (let ((rperms (list (parse-integer (subseq perms-str 0 1) :radix 8)
+		      (parse-integer (subseq perms-str 1 2) :radix 8)
+		      (parse-integer (subseq perms-str 2 3) :radix 8)))
+	(state 0)
+	(unixread 4)
+	(unixwrite 2)
+	(unixexec 1)
+	(perms nil))
+    (dolist (x rperms)
+      (if (not (< (- x unixread) 0))
+	  (setf perms (append perms 
+			      (case state
+				(0 (list :user-read))
+				(1 (list :group-read))
+				(2 (list :other-read))))
+		x (- x unixread)))
+      (if (not (< (- x unixwrite) 0))
+	  (setf perms (append perms
+			      (case state
+				(0 (list :user-write))
+				(1 (list :group-write))
+				(2 (list :other-write))))
+		x (- x unixwrite)))
+      (if (equal x unixexec)
+	  (setf perms (append perms
+			      (case state
+				(0 (list :user-exec))
+				(1 (list :group-exec))
+				(2 (list :other-exec))))))
+      (incf state))
+    perms))
+
 (defun pl-install (args)
   (if (listp args)
       (let* ((filename (varhdl (getf args :file)))
@@ -33,6 +83,8 @@
 	    (setf target (get-var 'bindir)))
 	(if (nilp mode)
 	    (setf mode (get-var 'default-mode)))
+	(if (stringp mode)
+	    (setf mode (number-to-perms mode)))
 	(setf cleantgt (subseq target 1))
 	(setf destpath (cl-fad:canonical-pathname
 			(make-pathname :directory (list :absolute cleantgt) :name filename)))
@@ -41,7 +93,7 @@
 	      (format t ":install ~A -> ~A (~A)~%"
 		      filename
 		      destpath
-		      mode)
+		      (perms-to-number mode))
 	      (cl-fad:copy-file
 	       filename
 	       (concatenate 'string
